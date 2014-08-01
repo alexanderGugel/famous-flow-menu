b345e07c33fff24d0edd7b3f91d65ab49fe24b9b
Alexander Gugel <alexander.gugel@gmail.com>
2014-08-01 14:27:19 -0700

    Add icon

--- app/src/views/NavView.js
+++ app/src/views/NavView.js
@@ -5,6 +5,8 @@ define(function(require, exports, module) {
     var Transform = require('famous/core/Transform');
     var StateModifier = require('famous/modifiers/StateModifier');
     var Easing = require('famous/transitions/Easing');
+    var ImageSurface = require('famous/surfaces/ImageSurface');
+    var SequentialLayout = require('famous/views/SequentialLayout');
 
     /*
      * @name FlowMenuNavView
@@ -18,16 +20,25 @@ define(function(require, exports, module) {
         this._focused = true;
 
         this.stateModifier = new StateModifier({
+            origin: [0.5, 0],
+            align: [0.5, 0]
         });
 
-        this.nav = this.add(this.stateModifier);
+        this.sequentialLayout = new SequentialLayout();
 
-        this.nav.add(new Surface({
-          content: 'Test',
-          properties: {
-              zIndex: -10
-          }
-        }));
+        this.nav = this.add(this.stateModifier).add(this.sequentialLayout);
+
+        this.sequentialLayout.sequenceFrom([new Surface({
+            size: [30, 30],
+            classes: ['ion-arrow-left-a', 'icon'],
+            properties: {
+                zIndex: -10,
+                fontSize: '30px',
+                // marign: '5px',
+                textShadow: '-2px 2px 0px rgba(255, 255, 255, 0.1)',
+                color: 'rgba(255, 255, 255, 0.9)'
+            }
+        })]);
     }
 
     FlowMenuNavView.prototype = Object.create(View.prototype);
