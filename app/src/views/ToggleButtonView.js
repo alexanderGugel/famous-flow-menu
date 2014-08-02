7084b14fd4402a614b09aee3b80d61385d9f1709
Alexander Gugel <alexander.gugel@gmail.com>
2014-08-01 20:15:19 -0700

    Refactor plus icon

--- app/src/views/ToggleButtonView.js
+++ app/src/views/ToggleButtonView.js
@@ -7,7 +7,7 @@ define(function(require, exports, module) {
     var Easing = require('famous/transitions/Easing');
 
     /*
-     * @name ToggleButtonView.js
+     * @name ToggleButtonView
      * @constructor
      * @description
      */
@@ -22,48 +22,42 @@ define(function(require, exports, module) {
             boxShadow: '-2px 2px 0px 0px rgba(0, 0, 0, 0.1)'
         };
 
+        var verticalBarSize = [30, 7];
+
         this.verticalBarSurface = new Surface({
-            size: [30, 7],
+            size: verticalBarSize,
             properties: plusStyles
         });
 
         this.horizontalBarSurface = new Surface({
-            size: [7, 30],
+            size: verticalBarSize.reverse(),
             properties: plusStyles
         });
 
-        this.verticalBarSurface.on('click', function() {
+        this.verticalBarSurface.pipe(this._eventInput);
+        this.horizontalBarSurface.pipe(this._eventInput);
+
+        this._eventInput.on('click', function() {
             this._eventOutput.emit('toggleRequested');
         }.bind(this));
-
-
-        this.horizontalBarSurface.on('click', function() {
-            this._eventOutput.emit('toggleRequested');
-        }.bind(this));
-
 
         this.on('toggle', function() {
-            this.verticalBarModifier.setTransform(Transform.scale(0.4, 1, 1), {
-                curve: Easing.inQuin,
-                duration: 100
-            });
-            this.verticalBarModifier.setTransform(Transform.scale(1, 1, 1), {
+            var showTransition = {
                 curve: Easing.outQuin,
                 duration: 100
-            });
-            this.horizontalBarModifier.setTransform(Transform.scale(1, 0.4, 1), {
+            };
+            var hideTransition = {
                 curve: Easing.inQuin,
                 duration: 100
-            });
-            this.horizontalBarModifier.setTransform(Transform.scale(1, 1, 1), {
-                curve: Easing.outQuin,
-                duration: 100
-            });
+            };
+            this.verticalBarModifier.setTransform(Transform.scale(0.4, 1, 1), hideTransition);
+            this.verticalBarModifier.setTransform(Transform.scale(1, 1, 1), showTransition);
+            this.horizontalBarModifier.setTransform(Transform.scale(1, 0.4, 1), hideTransition);
+            this.horizontalBarModifier.setTransform(Transform.scale(1, 1, 1), showTransition);
             this.centerModifier.setTransform(Transform.rotateZ(this._focused ? Math.PI/4 : 0), {
                 curve: Easing.outElastic,
                 duration: 300
-            })
-            console.log('Toggle button!');
+            });
             this._focused = !this._focused;
         });
 
@@ -72,8 +66,8 @@ define(function(require, exports, module) {
             align: [0.5, 0.5]
         });
 
-        this.verticalBarModifier = new StateModifier({});
-        this.horizontalBarModifier = new StateModifier({});
+        this.verticalBarModifier = new StateModifier();
+        this.horizontalBarModifier = new StateModifier();
 
         this.plusIcon = this.add(this.centerModifier);
         this.plusIcon.add(this.verticalBarModifier).add(this.verticalBarSurface);
